da0d9a15d955c751d0d6fa22087b89ca11f091e9
ehsui <164998376+ehsui@users.noreply.github.com>
2024-03-28 14:52:12 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int a, b;
+	do {
+		scanf("%d%d", &a, &b);
+		if (a!= 0 && b!= 0) {
+
+			printf("%d\n", a+b);
+
+		}
+	} while (a + b != 0);
+
+
+	return 0;
+}
