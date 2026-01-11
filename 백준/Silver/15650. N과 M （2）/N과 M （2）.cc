ad5499859c02889e959b1d12040064d8cd85b159
ehsui <164998376+ehsui@users.noreply.github.com>
2026-01-11 17:14:38 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+bool isUsed[10] = {false};
+int N, M, arr[10], selected[10];
+void back(int depth, int idx);
+
+int main()
+{
+
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++)
+        arr[i] = i;
+
+    back(0, 1);
+
+    return 0;
+}
+
+void back(int depth, int idx)
+{
+
+    if (depth == M)
+    {
+        for (int i = 0; i < M - 1; i++)
+        {
+            cout << selected[i] << " ";
+        }
+        cout << selected[M - 1] << "\n";
+
+        return;
+    }
+
+    for (int cur = idx; cur <= N; cur++)
+    {
+        selected[depth] = arr[cur];
+        back(depth + 1, cur + 1);
+    }
+}
