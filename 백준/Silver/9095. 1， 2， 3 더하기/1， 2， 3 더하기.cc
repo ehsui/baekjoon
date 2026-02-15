62881c7ffb2d999af9d80b8fc7c76c8e4aa9c613
ehsui <164998376+ehsui@users.noreply.github.com>
2026-02-15 17:29:55 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,32 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
+int main()
+{
 
-int main(){
-	int T, n;
+    int T, n, dp[11];
 
-	cin >> T;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
 
-	for (int t = 0; t < T; t++) {
+    cin >> T;
+    for (int t = 0; t < T; t++)
+    {
+        cin >> n;
+        for (int i = 4; i <= n; i++)
+        {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        }
 
-		cin >> n;
-
-		vector<int> dp(n + 1);
-
-		dp[0] = 0;
-		dp[1] = 1;
-		dp[2] = 2; //11, 2
-		dp[3] = 4; //111, 12, 21, 3
-
-		for (int i = 4; i < n + 1; i++) {
-			dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
-		}
-
-		cout << dp[n] << '\n';
-	}
-
-	return 0;
+        cout << dp[n] << "\n";
+    }
 }
