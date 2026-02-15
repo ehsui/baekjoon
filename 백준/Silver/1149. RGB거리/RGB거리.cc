2ca96e5c50c1ea803d0e3ca54e624cef9ec3fb82
ehsui <164998376+ehsui@users.noreply.github.com>
2026-02-15 17:52:59 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -4,33 +4,34 @@ using namespace std;
 
 int main()
 {
-    int N;
 
+    int N;
     cin >> N;
 
-    int (*cost)[3] = new int[N + 1][3];
-    int (*d)[3] = new int[N + 1][3];
+    int cost[1000][3];
+    int dp[1000][3];
+    int answer = 0;
 
     for (int i = 1; i <= N; i++)
     {
+
         cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
     }
 
-    d[1][0] = cost[1][0];
-    d[1][1] = cost[1][1];
-    d[1][2] = cost[1][2];
+    dp[1][0] = cost[1][0];
+    dp[1][1] = cost[1][1];
+    dp[1][2] = cost[1][2];
 
     for (int i = 2; i <= N; i++)
     {
-        d[i][0] = cost[i][0] + min(d[i - 1][1], d[i - 1][2]);
-        d[i][1] = cost[i][1] + min(d[i - 1][0], d[i - 1][2]);
-        d[i][2] = cost[i][2] + min(d[i - 1][0], d[i - 1][1]);
+        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
     }
 
-    int tmp = min(d[N][1], d[N][2]);
-    int answer = min(tmp, d[N][0]);
+    int tmp = min(dp[N][0], dp[N][1]);
+    answer = min(tmp, dp[N][2]);
 
     cout << answer;
-
     return 0;
 }
