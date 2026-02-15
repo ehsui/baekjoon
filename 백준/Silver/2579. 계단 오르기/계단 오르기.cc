103d4c7faff66601cc451dcadad335d191fa0033
ehsui <164998376+ehsui@users.noreply.github.com>
2026-02-15 17:10:07 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,42 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
+int dp[301];
 
+int main()
+{
 
-int main(){
-	int n;
+    int N;
 
-	cin >> n;
-	vector<int> stairs(n);
-	vector <int> dp(n);
+    cin >> N;
 
-	for (int i = 0; i < n; i++) {
-		cin >> stairs[i];
-	}
+    vector<int> stairs(N + 1);
 
-	dp[0] = stairs[0]; //1칸인 경우 최댓값
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> stairs[i];
+    }
 
-	if (n == 0) {
-		cout << dp[0];
-		return 0;
-	}
+    dp[1] = stairs[1];
+    dp[2] = stairs[1] + stairs[2];
+    dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
 
-	dp[1] = stairs[0] + stairs[1]; //2칸인 경우 최댓값
+    for (int i = 4; i <= N; i++)
+    {
+        dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
+    }
 
-	if (n == 1) {
-		cout << dp[1];
-		return 0;
-	}
-
-	dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2]); //3칸인 경우 최댓값
-
-	for (int i = 3; i < n; i++) {
-		dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
-	}
-
-	cout << dp[n - 1];
-
-	return 0;
+    cout << dp[N];
+    return 0;
 }
