0891fe354d89d5e5d6d4a9333cc43fd05a308023
ehsui <164998376+ehsui@users.noreply.github.com>
2026-02-16 17:09:41 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -2,64 +2,85 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+
 using namespace std;
 
-const int MAX = 1001;
-bool visited[MAX];
-bool visited2[MAX];
-vector<int> adj[MAX];
-queue <int> Q;
+void BFS(int v);
+void DFS(int v);
 
-void DFS(int cur) {
-    visited[cur] = true;
-    cout << cur << " ";
+int N, M, V;
+vector<vector<int>> graph;
+vector<bool> isvisited;
 
-    for (int i = 0; i < adj[cur].size(); i++) {
-        int next = adj[cur][i];
-        if (visited[next]) continue;
-        DFS(next);
-    }
-}
-
-void BFS(int cur) {
-    visited2[cur] = true;
-    Q.push(cur);
-
-    while (!Q.empty()) {
-
-        int next = Q.front();
-        Q.pop();
-        cout << next << " ";
-
-        for (int i = 0; i < adj[next].size(); i++) {
-            int x = adj[next][i];
-            if (!visited2[x]) {
-                Q.push(x);
-                visited2[x] = true;
-            }
-        }
-    }
-}
-
-int main() {
-    int N, M, V;
-
+int main()
+{
     cin >> N >> M >> V;
 
-    for (int i = 0; i < M; i++) {
-        int input1, input2;
-        cin >> input1 >> input2;
-        adj[input1].push_back(input2);
-        adj[input2].push_back(input1);
+    graph.resize(N + 1, vector<int>());
+    isvisited.resize(N + 1, false);
+
+    int x, y;
+    for (int i = 0; i < M; i++)
+    {
+        cin >> x >> y;
+
+        graph[x].push_back(y);
+        graph[y].push_back(x);
     }
-    
-    for (int j = 0; j < N; j++) {
-        sort(adj[j + 1].begin(), adj[j + 1].end());
+
+    for (int i = 1; i <= N; i++)
+    {
+        sort(graph[i].begin(), graph[i].end());
     }
 
     DFS(V);
     cout << "\n";
+
+    for (int i = 1; i <= N; i++)
+    {
+        isvisited[i] = false;
+    }
     BFS(V);
 
     return 0;
+}
+
+void DFS(int v)
+{
+    isvisited[v] = true;
+    cout << v << " ";
+    for (int i = 0; i < graph[v].size(); i++)
+    {
+        int next = graph[v][i];
+
+        if (!isvisited[next])
+        {
+            DFS(next);
+        }
+    }
+}
+
+void BFS(int v)
+{
+    queue<int> q;
+
+    q.push(v);
+    isvisited[v] = true;
+
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+        cout << cur << " ";
+
+        for (int i = 0; i < graph[cur].size(); i++)
+        {
+            int next = graph[cur][i];
+            if (!isvisited[next])
+            {
+                isvisited[next] = true;
+                q.push(next);
+            }
+        }
+    }
 }
