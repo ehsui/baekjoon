97d3a7ffaf35fcac91a9047f865fcd6bef986e5f
ehsui <164998376+ehsui@users.noreply.github.com>
2026-02-17 08:40:51 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -1,35 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-vector<int> memo;
 
 int main()
 {
-    int N, max_value;
 
+    int N;
     cin >> N;
-    vector<int> arr = vector<int>(N + 1);
-    memo = vector<int>(N + 1);
-    fill(memo.begin(), memo.end(), -1);
 
+    vector<int> cards(N + 1);
+    vector<int> dp(N + 1);
+
+    // i가 카드 개수, cards[i] 값이 카드 가격
     for (int i = 1; i <= N; i++)
     {
-        cin >> arr[i];
+        cin >> cards[i];
+        dp[i] = cards[i];
     }
 
-    memo[0] = 0;
-    memo[1] = arr[1];
-
     for (int i = 1; i <= N; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j < i; j++)
         {
-            memo[i] = max(memo[i], memo[i - j] + arr[j]);
+            dp[i] = max(dp[i], dp[i - j] + cards[j]);
         }
     }
 
-    cout << memo[N];
+    cout << dp[N];
     return 0;
 }
