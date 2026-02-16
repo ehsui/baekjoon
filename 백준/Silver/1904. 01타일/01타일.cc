daa27a10d7f4cc8191b37619cdc6f62bd459d09f
ehsui <164998376+ehsui@users.noreply.github.com>
2026-02-16 20:07:08 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -1,31 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-long long DP(int k);
-
 int main()
 {
+
     int N;
-    long long result;
-
     cin >> N;
-    vector<long long> memory = vector<long long>(N + 1);
-    fill(memory.begin(), memory.end(), -1);
 
-    // 기저
-    memory[0] = 0;
-    memory[1] = 1;
-    memory[2] = 2;
+    vector<int> dp(N + 1);
 
+    dp[1] = 1;
+    dp[2] = 2;
     for (int i = 3; i <= N; i++)
     {
-        memory[i] = (memory[i - 1] + memory[i - 2]) % 15746;
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
     }
 
-    cout << memory[N];
-
+    cout << dp[N];
     return 0;
 }
