a375885b5dfc4539ea2e305ac7fcbac82cef766f
ehsui <164998376+ehsui@users.noreply.github.com>
2026-02-16 15:50:33 +0900

    [Silver I] Title: 정수 삼각형, Time: 36 ms, Memory: 4000 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,62 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    int N;
 
-    cin >> N;
+    int n;
 
-    int **dp;
-    int **arr;
+    cin >> n;
 
-    dp = new int *[N + 1];
-    arr = new int *[N + 1];
+    vector<vector<int>> vec(n + 1, vector<int>(n + 1));
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1));
 
-    for (int i = 0; i < N + 1; i++)
+    for (int i = 0; i < n; i++)
     {
-        dp[i] = new int[N + 1];
-        arr[i] = new int[N + 1];
-    }
-
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= i; j++)
+        for (int j = 0; j <= i; j++)
         {
-            cin >> arr[i][j];
+            cin >> vec[i][j];
         }
     }
 
-    dp[1][1] = arr[1][1];
-
-    for (int i = 2; i <= N; i++)
+    dp[0][0] = vec[0][0];
+    for (int i = 1; i < n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 0; j <= i; j++)
         {
-            if (j == 1)
+            if (j == 0)
             {
-                dp[i][j] = arr[i][j] + dp[i - 1][j];
-            } // 고려할 부모가 오른쪽 하나
+                dp[i][j] = dp[i - 1][0] + vec[i][j];
+            }
             else if (j == i)
             {
-                dp[i][j] = arr[i][j] + dp[i - 1][j - 1];
-            } // 고려할 부모가 왼쪽 하나
+                dp[i][j] = dp[i - 1][j - 1] + vec[i][j];
+            }
             else
             {
-                dp[i][j] = max(arr[i][j] + dp[i - 1][j], arr[i][j] + dp[i - 1][j - 1]);
-            } // 둘 다 고려해야 함
+                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + vec[i][j];
+            }
         }
     }
 
-    int result = dp[N][1];
+    int max = *max_element(dp[n - 1].begin(), dp[n - 1].end());
+    cout << max;
 
-    for (int i = 2; i <= N; i++)
-    {
-        result = max(result, dp[N][i]);
-    }
-
-    cout << result;
     return 0;
 }
